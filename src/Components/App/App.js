d599d7acde9d98f84b702e3281721f1993009154
jrjunior72 <jrjunior72@hotmail.com>
2021-08-30 16:30:24 -0300

    first loading

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SmartCounter from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood</h1>
+      <Ifoodcounter/>
+    </>
+  );
+}
+
+export default App;
